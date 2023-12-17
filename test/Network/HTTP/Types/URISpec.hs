7af22aa0b06d289e5e470f089a38f9ac32d62b9c
Felix Paulusma <felix.paulusma@gmail.com>
2023-12-17 22:02:53 +0100

    URISpec: created newtype to not generate empty keys when generating queries. Also added a round trip test for 'Query <-> QueryText'

--- test/Network/HTTP/Types/URISpec.hs
+++ test/Network/HTTP/Types/URISpec.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Network.HTTP.Types.URISpec (main, spec) where
@@ -6,47 +10,16 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as BL
-import Data.Text (Text)
+import Data.Text as T (Text, null)
 import Debug.Trace (traceShow)
 import Test.Hspec
-import Test.QuickCheck (property, (==>))
+import Test.QuickCheck (Arbitrary (..), listOf, property, suchThat, (==>), Gen)
 import Test.QuickCheck.Instances ()
 
 import Network.HTTP.Types
 
 main :: IO ()
 main = hspec spec
-
-nonEmptyPaths :: Query -> Query
-nonEmptyPaths = filter (not . B.null . fst)
-
-propEncodeDecodePath :: ([Text], Query) -> Bool
-propEncodeDecodePath (p', q') =
-    let x = BL.toStrict . B.toLazyByteString $ encodePath a b
-        y = decodePath x
-        z = y == (a, b)
-     in if z then z else traceShow (a, b, x, y) z
-  where
-    a = if p' == [""] then [] else p'
-    b = nonEmptyPaths q'
-
-propEncodeDecodeQuery :: Query -> Bool
-propEncodeDecodeQuery q' =
-    q == parseQuery (renderQuery True q)
-  where
-    q = nonEmptyPaths q'
-
-propQueryQuestionMark :: (Bool, Query) -> Bool
-propQueryQuestionMark (useQuestionMark, query) = actual == expected
-  where
-    actual = case B8.uncons $ renderQuery useQuestionMark query of
-        Nothing -> False
-        Just ('?', _) -> True
-        _ -> False
-    expected = case (useQuestionMark, null query) of
-        (False, _) -> False
-        (True, True) -> False
-        (True, False) -> True
 
 spec :: Spec
 spec = do
@@ -59,7 +32,8 @@ spec = do
     describe "encode/decode query" $ do
         it "is identity to encode and then decode" $
             property propEncodeDecodeQuery
-
+        it "is identity to convert to and from Text" $
+            property propConvertQueryText
         it "add ? in front of Query if and only if necessary" $
             property propQueryQuestionMark
 
@@ -108,3 +82,52 @@ spec = do
 
         it "returns value with '+' preserved" $ do
             parseQueryReplacePlus False "?a=b+c+d" `shouldBe` [("a", Just "b+c+d")]
+
+propEncodeDecodePath :: ([Text], QueryGen B.ByteString) -> Bool
+propEncodeDecodePath (p', QueryGen b) =
+    if z then z else traceShow (a, b, x, y) z
+  where
+    a = if p' == [""] then [] else p'
+    x = BL.toStrict . B.toLazyByteString $ encodePath a b
+    y = decodePath x
+    z = y == (a, b)
+
+propEncodeDecodeQuery :: QueryGen B.ByteString -> Bool
+propEncodeDecodeQuery (QueryGen q) =
+    q == parseQuery (renderQuery True q)
+
+propQueryQuestionMark :: (Bool, Query) -> Bool
+propQueryQuestionMark (useQuestionMark, query) =
+    actual == expected
+  where
+    actual = case B8.uncons $ renderQuery useQuestionMark query of
+        Just ('?', _) -> True
+        _ -> False
+    expected = useQuestionMark && not (Prelude.null query)
+
+propConvertQueryText :: QueryGen Text -> Bool
+propConvertQueryText (QueryGen q) =
+    q == (queryToQueryText . queryTextToQuery) q
+
+newtype QueryGenItem b = QueryGenItem (b, Maybe b)
+    deriving newtype Show
+newtype QueryGen b = QueryGen [(b, Maybe b)]
+    deriving newtype Show
+
+instance Arbitrary (QueryGenItem B.ByteString) where arbitrary = arbQueryGenItem B.null
+instance Arbitrary (QueryGenItem Text) where arbitrary = arbQueryGenItem T.null
+instance Arbitrary (QueryGen B.ByteString) where arbitrary = arbQueryGen
+instance Arbitrary (QueryGen Text) where arbitrary = arbQueryGen
+
+arbQueryGenItem :: Arbitrary a => (a -> Bool) -> Gen (QueryGenItem a)
+arbQueryGenItem p = do
+    k <- arbitrary `suchThat` (not . p)
+    v <- arbitrary
+    pure $ QueryGenItem (k, v)
+
+arbQueryGen :: Arbitrary (QueryGenItem a) => Gen (QueryGen a)
+arbQueryGen = do
+    items <- listOf arbitrary
+    pure . QueryGen $ go <$> items
+  where
+    go (QueryGenItem i) = i
