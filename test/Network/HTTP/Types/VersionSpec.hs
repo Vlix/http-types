2d488578a121d8f91723920845ed73f276a4d1a5
Felix Paulusma <felix.paulusma@gmail.com>
2023-12-24 21:40:27 +0100

    VersionSpec: reformatted tests

--- test/Network/HTTP/Types/VersionSpec.hs
+++ test/Network/HTTP/Types/VersionSpec.hs
@@ -11,18 +11,25 @@ main = hspec spec
 
 spec :: Spec
 spec =
-    describe "Regression tests" $ do
-        it "HTTP/0.9" $ do
-            http09 `shouldBe` HttpVersion 0 9
-            show http09 `shouldBe` "HTTP/0.9"
-        it "HTTP/1.0" $ do
-            http10 `shouldBe` HttpVersion 1 0
-            show http10 `shouldBe` "HTTP/1.0"
-        it "HTTP/1.1" $ do
-            http11 `shouldBe` HttpVersion 1 1
-            show http11 `shouldBe` "HTTP/1.1"
-        it "HTTP/2  " $ do
-            http20 `shouldBe` HttpVersion 2 0
-            show http20 `shouldBe` "HTTP/2.0"
+    describe "Regression tests" $
+        mapM_ checkVersion allVersions
+
+-- | [("Rendered", {constant}, {literal}, "Shown")]
+allVersions :: [(String, HttpVersion, HttpVersion, String)]
+allVersions =
+    [ ("HTTP/0.9", http09, HttpVersion 0 9, "HTTP/0.9")
+    , ("HTTP/1.0", http10, HttpVersion 1 0, "HTTP/1.0")
+    , ("HTTP/1.1", http11, HttpVersion 1 1, "HTTP/1.1")
+    , ("HTTP/2", http20, HttpVersion 2 0, "HTTP/2.0")
+    ]
+
+checkVersion :: (String, HttpVersion, HttpVersion, String) -> Spec
+checkVersion (msg, v1, v2, str) =
+    it (msg ++ extra) $ do
+        v1 `shouldBe` v2
+        show v1 `shouldBe` str
+  where
+    extra = replicate (len - length msg) ' '
+    len = maximum $ (\(a, _, _, _) -> length a) <$> allVersions
 
 -- it "parses to HTTP/3" $ http30 `shouldBe` HttpVersion 3 0
