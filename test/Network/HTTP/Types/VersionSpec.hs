ce17f4924b405699e64bcc7ad407b163bfcb577a
Felix Paulusma <felix.paulusma@gmail.com>
2024-01-07 00:29:00 +0100

    VersionSpec: cleanup

--- test/Network/HTTP/Types/VersionSpec.hs
+++ test/Network/HTTP/Types/VersionSpec.hs
@@ -20,16 +20,13 @@ allVersions =
     [ ("HTTP/0.9", http09, HttpVersion 0 9, "HTTP/0.9")
     , ("HTTP/1.0", http10, HttpVersion 1 0, "HTTP/1.0")
     , ("HTTP/1.1", http11, HttpVersion 1 1, "HTTP/1.1")
-    , ("HTTP/2", http20, HttpVersion 2 0, "HTTP/2.0")
+    , ("HTTP/2.0", http20, HttpVersion 2 0, "HTTP/2.0")
     ]
 
 checkVersion :: (String, HttpVersion, HttpVersion, String) -> Spec
 checkVersion (msg, v1, v2, str) =
-    it (msg ++ extra) $ do
+    it msg $ do
         v1 `shouldBe` v2
         show v1 `shouldBe` str
-  where
-    extra = replicate (len - length msg) ' '
-    len = maximum $ (\(a, _, _, _) -> length a) <$> allVersions
 
 -- it "parses to HTTP/3" $ http30 `shouldBe` HttpVersion 3 0
