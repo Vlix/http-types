debac10159e9770af5c37fe59580f58a7829ed1f
Felix Paulusma <felix.paulusma@gmail.com>
2026-06-10 17:30:21 +0200

    lots of benchmarking done

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -Wno-orphans -Wno-x-partial #-}
 
@@ -8,21 +10,33 @@ module Main where
 import Control.DeepSeq (NFData (..), force)
 import Control.Exception (evaluate)
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import Data.ByteString.Char8 (unpack)
 import qualified Data.ByteString.Char8 as B8
 import Data.CaseInsensitive (CI (..), foldCase, mk)
 import Data.Char (toLower)
 import Data.Functor ((<&>))
-import qualified Data.HashMap.Lazy as LHM
-import qualified Data.HashMap.Strict as HM
+
+-- import qualified Data.HashMap.Lazy as LHM
+-- import qualified Data.HashMap.Strict as HM
 import qualified Data.List as L
-import qualified Data.Map.Lazy as LM
-import qualified Data.Map.Strict as M
+
+-- import qualified Data.Map.Lazy as LM
+-- import qualified Data.Map.Strict as M
+-- import Data.Maybe (fromMaybe)
+-- import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import Network.HTTP.Header as H (
+    addHeader,
+    addHeaderFront,
     caseInsensitiveEq,
     encodeHeaderName,
     encodeHeaderNameLower,
+    fromList,
+    lookupHeader,
+    removeHeader,
+    setHeader,
+    setHeaderFront,
     unsafeParseHeaderName,
  )
 import Network.HTTP.Header.Constants
@@ -33,8 +47,8 @@ main :: IO ()
 main =
     defaultMain
         [ caseInsensitiveEqBench
-        , -- , bgroup "HeaderName" headerNameBenches
-          bgroup "HeaderMap" headerMapBenches
+        , bgroup "HeaderName" headerNameBenches
+        , bgroup "HeaderMap" headerMapBenches
         ]
 
 headerNameBenches :: [Benchmark]
@@ -99,6 +113,8 @@ createBench =
         , ("Average", averageDDG)
         ]
 
+-- We make a copy in the 'classicBench'mark since that more accurately
+-- describes what happens when actually writing it to an outgoing socket.
 encodeBench :: Benchmark
 encodeBench =
     benchBoth "Encode" classicBench newBench
@@ -112,7 +128,7 @@ encodeBench =
     classicBench =
         hdrs <&> \hdr ->
             pureEnv (mk hdr) $
-                bench (B8.unpack hdr) . nf original
+                bench (B8.unpack hdr) . nf (B8.copy . original)
     newBench f =
         hdrs <&> \hdr ->
             pureEnv (unsafeParseHeaderName hdr) $
@@ -132,7 +148,7 @@ encodeLowerBench =
     classicBench =
         hdrs <&> \hdr ->
             pureEnv (mk hdr) $
-                bench (B8.unpack hdr) . nf foldedCase
+                bench (B8.unpack hdr) . nf (B8.copy . foldedCase)
     newBench f =
         hdrs <&> \hdr ->
             pureEnv (unsafeParseHeaderName hdr) $
@@ -205,19 +221,6 @@ sequenceOfOperationsNoEncoding lookupHdrBS =
         [bench benchName $ nf (runAll (mk :: ByteString -> CI ByteString)) averageDDG]
     newBench f =
         [f benchName . bench benchName $ nf (runAll unsafeParseHeaderName) averageDDG]
-
--- parseHeaders :: String -> Benchmark
--- parseHeaders name =
---     env (readFile "bench/headers.txt") $ \bs ->
---         benchBoth name (classicBench bs) (newBench bs)
---   where
---     getHdrs f bs = do
-
---     classicBench =
---         bench "headers" . getHdrs mk
---     newBench bs f =
---         let n = "headers"
---          in f n . bench n $ getHdrs unsafeParseHeaderName bs
 
 shortSmall, longSmall, shortBig, longBig, averageDDG :: [ByteString]
 shortSmall = ["Age", "TE", "Tk", "Via"]
@@ -310,9 +313,15 @@ averageDDG =
 
 deriving instance Generic HeaderName
 instance NFData HeaderName
+deriving instance Generic Header
+instance NFData Header
+deriving instance Generic Headers
+instance NFData Headers
 
 deriving instance Generic Bitmap
 instance NFData Bitmap
+deriving instance Generic HashBitmap
+instance NFData HashBitmap
 deriving instance (Generic s) => Generic (HeaderNameException s)
 instance (Generic s, NFData s) => NFData (HeaderNameException s)
 
@@ -326,116 +335,181 @@ datatypeBenches =
         "Datatype"
         [ bgroup "List" $
             headerLists <&> runDataBench' False listActions
-        , bgroup "Map.Strict" $
-            headerLists <&> runDataBench mapActions
-        , -- , bgroup "Map.Lazy" $
-          --     headerLists <&> runDataBench lazyMapActions
-          bgroup "HashMap.Strict" $
-            headerLists <&> runDataBench hashMapActions
-            -- , bgroup "HashMap.Lazy" $
-            --     headerLists <&> runDataBench lazyHashMapActions
+        , bgroup "List.Header" $
+            headerLists <&> runDataBench listActionsNewHeader
+        , -- , bgroup "Vector" $
+          --     headerLists <&> runDataBench vectorActions
+          -- , bgroup "Map.Strict" $
+          --     headerLists <&> runDataBench mapActions
+          -- , -- , bgroup "Map.Lazy" $
+          --     headerLists <&> runDataBench _lazyMapActions
+          --   bgroup "HashMap.Strict" $
+          --     headerLists <&> runDataBench hashMapActions
+          -- , -- , bgroup "HashMap.Lazy" $
+          --     headerLists <&> runDataBench _lazyHashMapActions
+          bgroup "New Headers" $
+            headerLists <&> runDataBench newHeadersActions
         ]
 
 -- | Create the structure
-type Create a = [(HeaderName, ByteString)] -> a
+type Create a hdr = [hdr] -> a
 
 -- | Add a header
-type Add a = HeaderName -> ByteString -> a -> a
+type Add a hdr = hdr -> a -> a
 
 -- | Add a header and remove all other duplicates of the header
-type Replace a = HeaderName -> ByteString -> a -> a
+type Replace a hdr = hdr -> a -> a
 
 -- | Find the header and show first value
-type Lookup a b = HeaderName -> a -> Maybe b
-
--- | Find the header and if present change the value
-type Update a b = (b -> b) -> HeaderName -> a -> a
+type Lookup a b = HeaderName -> a -> [b]
 
 -- | Remove all instances of the header
 type Delete a = HeaderName -> a -> a
 
-data Actions a b = Actions
-    { aCreate :: Create a
-    , aAdd :: Add a
-    , aAddEnd :: Add a
-    , aReplace :: Replace a
+data Actions hdr a b = Actions
+    { aCreate :: Create a hdr
+    , aAdd :: Add a hdr
+    , aAddEnd :: Add a hdr
+    , aReplace :: Replace a hdr
+    , aReplaceEnd :: Replace a hdr
     , aLookup :: Lookup a b
-    , aUpdate :: Update a b
     , aDelete :: Delete a
-    , aUpdateF :: b -> b
+    , aFromByteString :: ByteString -> a
+    , aFromTuple :: HeaderName -> ByteString -> hdr
     }
 
-listActions :: Actions [(HeaderName, ByteString)] ByteString
+listActions :: Actions (HeaderName, ByteString) [(HeaderName, ByteString)] ByteString
 listActions =
     Actions
         { aCreate = id
-        , aAdd = listAdd
-        , aAddEnd = \k v xs -> xs <> [(k, v)]
+        , aAdd = (:)
+        , aAddEnd = \tup xs -> xs <> [tup]
         , aReplace = listReplace
-        , aLookup = L.lookup
-        , aUpdate = listUpdate
+        , aReplaceEnd = \tup@(k, _) xs -> listDelete k xs <> [tup]
+        , aLookup = listLookup
         , aDelete = listDelete
-        , aUpdateF = updateHeader
+        , aFromByteString = reverse . withFile (,) (:) []
+        , aFromTuple = (,)
         }
   where
-    listAdd k v xs = (k, v) : xs
-    listReplace k v xs = (k, v) : listDelete k xs
-    listUpdate _ _ [] = []
-    listUpdate f k ((a, b) : xs)
-        | a == k = (a, f b) : listDelete k xs
-        | otherwise = (a, b) : listUpdate f k xs
+    listReplace tup@(k, _) xs = tup : listDelete k xs
     listDelete k = filter $ (/= k) . fst
+    listLookup nm xs = snd <$> filter ((==) nm . fst) xs
 
-mapActions :: Actions (M.Map HeaderName [ByteString]) [ByteString]
-mapActions =
+listActionsNewHeader :: Actions Header [Header] ByteString
+listActionsNewHeader =
     Actions
-        { aCreate = M.fromListWith (<>) . fmap (\(a, b) -> (a, [b]))
-        , aAdd = \k -> M.insertWith (flip (<>)) k . pure
-        , aAddEnd = \k -> M.insertWith (<>) k . pure
-        , aReplace = \k -> M.insert k . pure
-        , aLookup = M.lookup
-        , aUpdate = M.adjust
-        , aDelete = M.delete
-        , aUpdateF = fmap updateHeader . take 1
+        { aCreate = id
+        , aAdd = (:)
+        , aAddEnd = \hdr xs -> xs <> [hdr]
+        , aReplace = listReplace
+        , aReplaceEnd = \hdr xs -> listDelete (headerName hdr) xs <> [hdr]
+        , aLookup = listLookup
+        , aDelete = listDelete
+        , aFromByteString = reverse . withFile Header (:) []
+        , aFromTuple = Header
+        }
+  where
+    listReplace hdr xs = hdr : listDelete (headerName hdr) xs
+    listDelete k = filter $ (/= k) . headerName
+    listLookup nm xs = headerValue <$> filter ((==) nm . headerName) xs
+
+{-
+-- Other types I've tried but found lacking in one way or another.
+--  * Maps lose sequentiality (which would mean you'd need a list or something
+--    extra to keep the order, which would defeat the performance gains)
+--  * Vector would be kind of ok, but adding would be slower/the same, but most
+--    most importantly, the lookup would not be any quicker.
+
+_vectorActions :: Actions (V.Vector Header) ByteString
+_vectorActions =
+    Actions
+        { aCreate = V.fromList . fmap (uncurry Header)
+        , aAdd = V.cons
+        , aAddEnd = flip V.snoc
+        , aReplace = \hdr ->
+            V.cons hdr . V.filter ((== (headerName hdr)) . headerName)
+        , aReplaceEnd = \hdr xs ->
+            V.filter ((== (headerName hdr)) . headerName) xs `V.snoc` hdr
+        , aLookup = \k ->
+            V.toList . fmap headerValue . V.filter ((== k) . headerName)
+        , aDelete = \k -> V.filter ((/= k) . headerName)
+        , aFromByteString = V.fromList . reverse . withFile (:) []
         }
 
-_lazyMapActions :: Actions (LM.Map HeaderName [ByteString]) [ByteString]
+_mapActions :: Actions (M.Map HeaderName [ByteString]) ByteString
+_mapActions =
+    Actions
+        { aCreate = M.fromListWith (<>) . fmap (\(a, b) -> (a, [b]))
+        , aAdd = \(Header k v) -> M.insertWith (flip (<>)) k [v]
+        , aAddEnd = addEnd
+        , aReplace = \(Header k v) -> M.insert k [v]
+        , aReplaceEnd = \(Header k v) -> M.insert k [v]
+        , aLookup = \k -> fromMaybe [] . M.lookup k
+        , aDelete = M.delete
+        , aFromByteString = withFile addEnd mempty
+        }
+  where
+    addEnd (Header k v) = M.insertWith (<>) k [v]
+
+_lazyMapActions :: Actions (LM.Map HeaderName [ByteString]) ByteString
 _lazyMapActions =
     Actions
         { aCreate = LM.fromListWith (<>) . fmap (\(a, b) -> (a, [b]))
-        , aAdd = \k -> LM.insertWith (flip (<>)) k . pure
-        , aAddEnd = \k -> LM.insertWith (<>) k . pure
-        , aReplace = \k -> LM.insert k . pure
-        , aLookup = LM.lookup
-        , aUpdate = LM.adjust
+        , aAdd = \(Header k v) -> LM.insertWith (flip (<>)) k [v]
+        , aAddEnd = addEnd
+        , aReplace = \(Header k v) -> LM.insert k [v]
+        , aReplaceEnd = \(Header k v) -> LM.insert k [v]
+        , aLookup = \k -> fromMaybe [] . LM.lookup k
         , aDelete = LM.delete
-        , aUpdateF = fmap updateHeader . take 1
+        , aFromByteString = withFile addEnd mempty
         }
+  where
+    addEnd (Header k v) = LM.insertWith (<>) k [v]
 
-hashMapActions :: Actions (HM.HashMap HeaderName [ByteString]) [ByteString]
-hashMapActions =
+_hashMapActions :: Actions (HM.HashMap HeaderName [ByteString]) ByteString
+_hashMapActions =
     Actions
         { aCreate = HM.fromListWith (<>) . fmap (\(a, b) -> (a, [b]))
-        , aAdd = \k -> HM.insertWith (flip (<>)) k . pure
-        , aAddEnd = \k -> HM.insertWith (<>) k . pure
-        , aReplace = \k -> HM.insert k . pure
-        , aLookup = HM.lookup
-        , aUpdate = HM.adjust
+        , aAdd = \(Header k v) -> HM.insertWith (flip (<>)) k [v]
+        , aAddEnd = addEnd
+        , aReplace = \(Header k v) -> HM.insert k [v]
+        , aReplaceEnd = \(Header k v) -> HM.insert k [v]
+        , aLookup = \k -> fromMaybe [] . HM.lookup k
         , aDelete = HM.delete
-        , aUpdateF = fmap updateHeader . take 1
+        , aFromByteString = withFile addEnd mempty
         }
+  where
+    addEnd (Header k v) = HM.insertWith (<>) k [v]
 
-_lazyHashMapActions :: Actions (LHM.HashMap HeaderName [ByteString]) [ByteString]
+_lazyHashMapActions :: Actions (LHM.HashMap HeaderName [ByteString]) ByteString
 _lazyHashMapActions =
     Actions
         { aCreate = LHM.fromListWith (<>) . fmap (\(a, b) -> (a, [b]))
-        , aAdd = \k -> LHM.insertWith (flip (<>)) k . pure
-        , aAddEnd = \k -> LHM.insertWith (<>) k . pure
-        , aReplace = \k -> LHM.insert k . pure
-        , aLookup = LHM.lookup
-        , aUpdate = LHM.adjust
+        , aAdd = \(Header k v) -> LHM.insertWith (flip (<>)) k [v]
+        , aAddEnd = addEnd
+        , aReplace = \(Header k v) -> LHM.insert k [v]
+        , aReplaceEnd = \(Header k v) -> LHM.insert k [v]
+        , aLookup = \k -> fromMaybe [] . LHM.lookup k
         , aDelete = LHM.delete
-        , aUpdateF = fmap updateHeader . take 1
+        , aFromByteString = withFile addEnd mempty
+        }
+  where
+    addEnd (Header k v) = LHM.insertWith (<>) k [v]
+ -}
+
+newHeadersActions :: Actions Header Headers ByteString
+newHeadersActions =
+    Actions
+        { aCreate = fromList
+        , aAdd = addHeaderFront
+        , aAddEnd = addHeader
+        , aReplace = setHeaderFront
+        , aReplaceEnd = setHeader
+        , aLookup = lookupHeader
+        , aDelete = removeHeader
+        , aFromByteString = fromList . reverse . withFile Header (:) []
+        , aFromTuple = Header
         }
 
 updateHeader :: ByteString -> ByteString
@@ -443,41 +517,59 @@ updateHeader = (<> ", text/html;q=0.3")
 
 headerLists :: [(String, [HeaderName])]
 headerLists =
-    [ ("One", [hAccept])
-    , -- , ("Two", [hAccept, hServer])
-      -- , ("Four", [hAccept, hContentDisposition, hServer, hVia])
-      ("Average", unsafeParseHeaderName <$> averageDDG)
+    [ ("Four", [hAccept, hContentDisposition, hServer, hVia])
+    , ("Average", unsafeParseHeaderName <$> averageDDG)
     ]
 
-runDataBench :: (NFData a, NFData b) => Actions a b -> (String, [HeaderName]) -> Benchmark
+runDataBench :: (NFData a, NFData b, NFData hdr) => Actions hdr a b -> (String, [HeaderName]) -> Benchmark
 runDataBench = runDataBench' True
 
-runDataBench' :: (NFData a, NFData b) => Bool -> Actions a b -> (String, [HeaderName]) -> Benchmark
+runDataBench' :: (NFData a, NFData b, NFData hdr) => Bool -> Actions hdr a b -> (String, [HeaderName]) -> Benchmark
 runDataBench' shouldCompare actions (testName, hdrs) =
     bgroup
         testName
-        [ pureEnv hdrsList $ \hs ->
+        [ pureEnv (uncurry toHeader <$> hdrsList) $ \hs ->
             comp "Create" $ bench "Create" $ nf (aCreate actions) hs
-        , prepHdrs "Add Front" $ \ ~(hs, _) ->
-            whnf (aAdd actions hName "99") hs
-        , prepHdrs "Add End" $ \ ~(hs, _) ->
-            nf (aAddEnd actions hName "99") hs
-        , prepHdrs "Replace" $ \ ~(hs, _) ->
-            nf (aReplace actions hName "99") hs
-        , prepHdrs "Lookup First" $ \ ~(hs, firstHdr) ->
-            whnf (aLookup actions firstHdr) hs
-        , prepHdrs "Lookup Missing" $ \ ~(hs, _) ->
-            whnf (aLookup actions hName) hs
-        , prepHdrs "Update First" $ \ ~(hs, firstHdr) ->
-            nf (aUpdate actions (aUpdateF actions) firstHdr) hs
-        , prepHdrs "Update Missing" $ \ ~(hs, _) ->
-            nf (aUpdate actions (aUpdateF actions) hName) hs
-        , prepHdrs "Delete First" $ \ ~(hs, firstHdr) ->
-            nf (aDelete actions firstHdr) hs
-        , prepHdrs "Delete Missing" $ \ ~(hs, _) ->
-            nf (aDelete actions hName) hs
+        , -- , prepHdrs "Add Front Missing" $ \ ~(hs, _, _) ->
+          --     nf (aAdd actions $ hName >: "99") hs
+          prepHdrs "Add End Missing" $ \ ~(hs, _, _) ->
+            nf (aAddEnd actions $ toHeader hName "99") hs
+        , -- , prepHdrs "Add Front Middle" $ \ ~(hs, _, middleHdr) ->
+          --     nf (aAdd actions $ toHeader (middleHdr, "99")) hs
+          prepHdrs "Add End Middle" $ \ ~(hs, _, middleHdr) ->
+            nf (aAddEnd actions $ toHeader middleHdr "99") hs
+        , prepHdrs "Replace Missing" $ \ ~(hs, _, _) ->
+            nf (aReplace actions $ toHeader hName "99") hs
+        , prepHdrs "Replace Exists" $ \ ~(hs, _, xFrameHdr) ->
+            nf (aReplace actions $ toHeader xFrameHdr "99") hs
+        , -- , prepHdrs "Lookup First" $ \ ~(hs, firstHdr, _) ->
+          --     nf (aLookup actions firstHdr) hs
+          -- , prepHdrs "Lookup Middle" $ \ ~(hs, _, middleHdr) ->
+          --     nf (aLookup actions middleHdr) hs
+          prepHdrs "Lookup Missing" $ \ ~(hs, _, _) ->
+            nf (aLookup actions hName) hs
+        , -- , prepHdrs "Delete First" $ \ ~(hs, firstHdr, _) ->
+          --     nf (aDelete actions firstHdr) hs
+          -- , prepHdrs "Delete Missing" $ \ ~(hs, _, _) ->
+          --     nf (aDelete actions hName) hs
+          prepHdrs "Everything" $ \ ~(hs, firstHdr, middleHdr) ->
+            nf
+                ( \headers ->
+                    let newHdrs =
+                            aAddEnd actions (toHeader firstHdr "everything")
+                                -- . aDelete actions firstHdr
+                                . aReplace actions (toHeader hDate "other date")
+                                . aReplaceEnd actions (toHeader firstHdr "new val")
+                                . aReplaceEnd actions (toHeader hRetryAfter "hehe")
+                                $ headers
+                     in aLookup actions hRange newHdrs <> aLookup actions middleHdr newHdrs
+                )
+                hs
+        , env (B.readFile "bench/headers.txt") $
+            comp "File" . bench "File" . nf (aFromByteString actions)
         ]
   where
+    toHeader = aFromTuple actions
     comp name =
         if shouldCompare
             then bcompare ("List." <> testName <> "." <> name)
@@ -486,8 +578,12 @@ runDataBench' shouldCompare actions (testName, hdrs) =
     hdrsList =
         zip hdrs $ B8.pack . show <$> [0 :: Int ..]
     prepHdrs name f =
-        pureEnv (aCreate actions hdrsList, head hdrs) $
-            comp name . bench name . f
+        pureEnv
+            ( aCreate actions $ uncurry toHeader <$> hdrsList
+            , head hdrs
+            , unsafeParseHeaderName "X-Frame-Options"
+            )
+            $ comp name . bench name . f
 
 pureEnv :: (NFData env) => env -> (env -> Benchmark) -> Benchmark
 pureEnv x = env (evaluate $ force x)
@@ -501,7 +597,7 @@ caseInsensitiveEqBench =
         , pureEnv ("Close" :: ByteString) $
             bcompare (topName <> ".BS")
                 . bench "Map"
-                . nf (\bs -> B8.map (toLower) bs == "close")
+                . nf (\bs -> B8.map toLower bs == "close")
         , pureEnv ("Close" :: ByteString) $
             bcompare (topName <> ".BS")
                 . bench "Custom"
@@ -509,3 +605,21 @@ caseInsensitiveEqBench =
         ]
   where
     topName = "Case insensitive EQ"
+
+withFile :: forall a b. (HeaderName -> ByteString -> b) -> (b -> a -> a) -> a -> ByteString -> a
+withFile f add =
+    loop
+  where
+    loop :: a -> ByteString -> a
+    loop !acc rest =
+        case B.break (== 0x0A) rest of
+            ("", "") -> acc
+            (final, "") ->
+                add (fromLine final) acc
+            (line, more) ->
+                loop (add (fromLine line) acc) $ B.drop 1 more
+      where
+        fromLine bs =
+            case B.break (== 0x3A) bs of
+                (_, "") -> error $ "BAD LINE: " <> B8.unpack bs
+                (k, v) -> unsafeParseHeaderName k `f` B.drop 1 v
